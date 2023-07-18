92b268f208ccb8ecc286fcbf7b7ea722f243279c
Julio-Maldonado <julioharlingen@gmail.com>
2023-07-17 18:10:48 -0700

    Adding first file

---
+++ main.py
@@ -0,0 +1,14 @@
+# main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+  
+
+@app.get("/test")
+async def root():
+    return {"message": "Hello test"}
